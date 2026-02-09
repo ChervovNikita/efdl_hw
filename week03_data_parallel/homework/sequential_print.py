34c8dc041699f0b4f37e8dc69a88732ef2631879
Nikita Chervov <gcnikitachervov@gmail.com>
2026-02-09 10:58:30 +0000

    hw3: 1, with report

--- week03_data_parallel/homework/sequential_print.py
+++ week03_data_parallel/homework/sequential_print.py
@@ -1,5 +1,5 @@
 import os
-
+import torch
 import torch.distributed as dist
 
 
@@ -25,7 +25,29 @@ def run_sequential(rank, size, num_iter=10):
     ```
     """
 
-    pass
+    buffer = torch.tensor(rank)
+
+    for _ in range(num_iter):
+        for i in range(rank):
+            dist.recv(buffer, i)
+        print(f"Process {rank}")
+        sends = []
+        for i in range(rank+1, size):
+            sends.append(dist.isend(buffer, i))
+        for send in sends:
+            send.wait()
+
+        for i in range(rank+1, size):
+            dist.recv(buffer, i)
+        print(f"Process {rank}")
+        sends = []
+        for i in range(rank):
+            sends.append(dist.isend(buffer, i))
+        for send in sends:
+            send.wait()
+
+        if rank == 0 and _ != num_iter - 1:
+            print("---")
 
 
 if __name__ == "__main__":
