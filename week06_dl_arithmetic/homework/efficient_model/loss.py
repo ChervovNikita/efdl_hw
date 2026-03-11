d91cc3d3017578ccc43d7a23a70102adbfbbd5af
Nikita Chervov <gcnikitachervov@gmail.com>
2026-03-11 14:12:19 +0000

    hw6: 0.5

--- week06_dl_arithmetic/homework/efficient_model/loss.py
+++ week06_dl_arithmetic/homework/efficient_model/loss.py
@@ -4,6 +4,7 @@ Cross Entropy Loss for Causal LM
 
 import torch
 import torch.nn as nn
+from liger_kernel.transformers import LigerFusedLinearCrossEntropyLoss
 
 
 class CrossEntropyLoss(nn.Module):
@@ -14,7 +15,10 @@ class CrossEntropyLoss(nn.Module):
     def __init__(self, ignore_index: int = -100):
         super().__init__()
         self.ignore_index = ignore_index
+        self.loss_fn = LigerFusedLinearCrossEntropyLoss(ignore_index=ignore_index)
 
-    def forward(self,) -> torch.Tensor:
+
+    def forward(self, hidden_states, lm_head_weight, labels) -> torch.Tensor:
         # TODO: Implement forward pass
-        raise NotImplementedError("TODO: Implement forward pass")
+        bs, seq_len, hidden_dim = hidden_states.shape
+        return self.loss_fn(lin_weight=lm_head_weight, _input=hidden_states.reshape(bs * seq_len, hidden_dim), target=labels.reshape(-1))
