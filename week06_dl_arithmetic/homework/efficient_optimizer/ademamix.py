f99973a5792cd6f619830a6b40a7d0f1497cc80e
Nikita Chervov <gcnikitachervov@gmail.com>
2026-03-12 18:34:57 +0000

    t

--- week06_dl_arithmetic/homework/efficient_optimizer/ademamix.py
+++ week06_dl_arithmetic/homework/efficient_optimizer/ademamix.py
@@ -4,29 +4,28 @@ import torch
 from torch import Tensor
 from torch.distributed.tensor import DTensor
 from torch.optim import Optimizer
+from typing import Optional
  
 # HINT: you may want to change these functions
 def linear_warmup_scheduler(step, alpha_end, alpha_start=0, warmup=1):
-    if step < warmup:
-        a = step / float(warmup)
-        return (1.0-a) * alpha_start + a * alpha_end
-    return alpha_end
+    return torch.where(
+        step < warmup,
+        (1.0-step / float(warmup)) * alpha_start + step / float(warmup) * alpha_end,
+        alpha_end
+    )
 
 
 def linear_hl_warmup_scheduler(step, beta_end, beta_start=0, warmup=1):
-
     def f(beta, eps=1e-8):
-        return math.log(0.5)/math.log(beta+eps)-1
+        return math.log(0.5)/torch.log(beta+eps)-1
 
-    def f_inv(t):
-        return math.pow(0.5, 1/(t+1))
+    return torch.where(
+        step < warmup,
+        torch.pow(0.5, 1/(((1.0-step / float(warmup)) * f(beta_start) + step / float(warmup) * f(beta_end))+1)),
+        beta_end
+    )
 
-    if step < warmup:
-        a = step / float(warmup)
-        return f_inv((1.0-a) * f(beta_start) + a * f(beta_end))
-    return beta_end
- 
- 
+
 @torch.compile(fullgraph=True) # you can comment out this line for subtask 1
 def ademamix_foreach_fn(
     params: list[Tensor],
@@ -34,19 +33,39 @@ def ademamix_foreach_fn(
     exp_avg_fasts: list[Tensor],
     exp_avg_slows: list[Tensor],
     exp_avg_sqs: list[Tensor],
-    bias_correction1: Tensor,
-    bias_correction2: Tensor,
-    alphas: Tensor,
-    beta3: Tensor,
-    beta1: float, beta2: float,
+    # bias_correction1: Tensor,
+    # bias_correction2: Tensor,
+    # alphas: Tensor,
+    # beta3: Tensor,
+    beta1_t: Tensor, beta2_t: Tensor,
     lr: float, lmbda: float, eps: float,
+    step: Tensor,
+    alpha_warmup: Optional[int],
+    beta3_warmup: Optional[int],
+    alpha_final: float,
+    beta3_final_t: Tensor,
+    beta1: float, beta2: float, beta3_final: float,
 ):
     # TODO: Replace per-tensor ops with torch._foreach_* equivalents:
     # torch._foreach_lerp_(exp_avgs, grads, 1 - beta1)
     # torch._foreach_mul_(exp_avg_sqs, beta2)
     # torch._foreach_addcmul_(exp_avg_sqs, grads, grads, 1 - beta2)
     # etc.
-    
+
+    step.add_(1)
+
+    bias_correction1 = (1 - torch.pow(beta1_t, step))
+    bias_correction2 = (1 - torch.pow(beta2_t, step))
+
+    if alpha_warmup is not None:
+        alphas = linear_warmup_scheduler(step, alpha_end=alpha_final, alpha_start=0, warmup=alpha_warmup)
+    else:
+        alphas = alpha_final
+    if beta3_warmup is not None:
+        beta3 = linear_hl_warmup_scheduler(step, beta_end=beta3_final_t, beta_start=beta1_t, warmup=beta3_warmup)
+    else:
+        beta3 = beta3_final_t
+
     exp_avg_fasts = torch._foreach_lerp_(exp_avg_fasts, grads, 1 - beta1)
 
     exp_avg_slows = torch._foreach_lerp_(exp_avg_slows, grads, 1 - beta3)
@@ -54,10 +73,13 @@ def ademamix_foreach_fn(
     exp_avg_sqs = torch._foreach_addcmul_(exp_avg_sqs, grads, grads, 1 - beta2)
 
     denoms = torch._foreach_add_(torch._foreach_div(torch._foreach_sqrt(exp_avg_sqs), torch.sqrt(bias_correction2)), eps)
-    updates = torch._foreach_div(torch._foreach_add_(torch._foreach_div(exp_avg_fasts, bias_correction1), torch._foreach_mul(exp_avg_slows, alphas)), denoms)
 
-    updates = torch._foreach_add_(updates, torch._foreach_mul(params, lmbda))
-    params = torch._foreach_add_(params, torch._foreach_mul(updates, -lr))
+    torch._foreach_add_(
+        params,
+        torch._foreach_mul(
+            torch._foreach_add_(torch._foreach_div(torch._foreach_add_(torch._foreach_div(exp_avg_fasts, bias_correction1), torch._foreach_mul(exp_avg_slows, alphas)), denoms), torch._foreach_mul(params, lmbda)),
+            -lr)
+    )
 
  
 class AdEMAMix(Optimizer):
@@ -124,15 +146,22 @@ class AdEMAMix(Optimizer):
             exp_avg_fasts: list[Tensor] = []
             exp_avg_slows: list[Tensor] = []
             exp_avg_sqs: list[Tensor] = []
-            bias_correction1: Tensor = 0.0
-            bias_correction2: Tensor = 0.0
-            alphas: Tensor = 0.0
-            beta3: Tensor = 0.0
+            # bias_correction1: Tensor = 0.0
+            # bias_correction2: Tensor = 0.0
+            # alphas: Tensor = 0.0
+            # beta3: Tensor = 0.0
 
             # TODO: prepare lists of tensors for 'foreach' step
             # as well as all other necessary inputs
             # HINT: (think about input types)
             
+            if group['params'][0].grad is None:
+                continue
+
+            if "step" not in group:
+                group["step"] = torch.tensor(0, device=group["params"][0].device, dtype=torch.int32)
+            group_step = group["step"]
+
             for p in group['params']:
                 params.append(p)
                 grads.append(p.grad)
@@ -145,23 +174,10 @@ class AdEMAMix(Optimizer):
                     exp_avg_fasts.append(torch.zeros_like(p, memory_format=torch.preserve_format))
                     exp_avg_slows.append(torch.zeros_like(p, memory_format=torch.preserve_format))
                     exp_avg_sqs.append(torch.zeros_like(p, memory_format=torch.preserve_format))
-                    self.state[p]['step'] = 0
-                self.state[p]['step'] += 1
-                bias_correction1 = 1 - beta1 ** self.state[p]['step']
-                bias_correction2 = 1 - beta2 ** self.state[p]['step']
-                
-                if alpha_warmup is not None:
-                    alphas = linear_warmup_scheduler(self.state[p]['step'], alpha_end=alpha_final, alpha_start=0, warmup=alpha_warmup)
-                else:
-                    alphas = alpha_final
 
-                if beta3_warmup is not None:
-                    beta3 = linear_hl_warmup_scheduler(self.state[p]['step'], beta_end=beta3_final, beta_start=beta1, warmup=beta3_warmup)
-                else:
-                    beta3 = beta3_final
-            
-            if params[0].grad is None:
-                continue
+            beta1_t = torch.tensor(beta1, device='cpu', dtype=torch.float64)
+            beta2_t = torch.tensor(beta2, device='cpu', dtype=torch.float64)
+            beta3_final_t = torch.tensor(beta3_final, device='cpu', dtype=torch.float64)
 
             ademamix_foreach_fn(
                 params=params,
@@ -169,18 +185,24 @@ class AdEMAMix(Optimizer):
                 exp_avg_fasts=exp_avg_fasts,
                 exp_avg_slows=exp_avg_slows,
                 exp_avg_sqs=exp_avg_sqs,
-                bias_correction1=torch.tensor(bias_correction1),
-                bias_correction2=torch.tensor(bias_correction2),
-                alphas=torch.tensor(alphas),
-                beta3=torch.tensor(beta3),
-                beta1=beta1,
-                beta2=beta2,
+                beta1_t=beta1_t,
+                beta2_t=beta2_t,
                 lr=lr, lmbda=lmbda, eps=eps,
+                step=group_step,
+                alpha_warmup=alpha_warmup,
+                beta3_warmup=beta3_warmup,
+                alpha_final=alpha_final,
+                beta3_final_t=beta3_final_t,
+                beta1 = beta1,
+                beta2 = beta2,
+                beta3_final = beta3_final,
             )
 
             for i, p in enumerate(group['params']):
                 self.state[p]['exp_avg_fast'] = exp_avg_fasts[i]
                 self.state[p]['exp_avg_slow'] = exp_avg_slows[i]
                 self.state[p]['exp_avg_sq'] = exp_avg_sqs[i]
+        
+            group['step'] = group_step
 
         return loss
